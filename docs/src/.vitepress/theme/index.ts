fc79b57677a4dc2d71a4a1788d90a76558925de6
sumu <2038035593@qq.com>
2025-08-21 23:27:11 +0800

    feat:[docs] 工作区内站点引入自定义布局

--- docs/src/.vitepress/theme/index.ts
+++ docs/src/.vitepress/theme/index.ts
@@ -2,15 +2,14 @@
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
+
 import './style.css'
+import MistLayoutProvider from "./components/MistLayoutProvider.vue";
+
 import Mist from "vitepress-theme-mist"; 
 export default {
   extends: Mist,
-  Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
-  },
+  Layout: MistLayoutProvider,
   enhanceApp({ app, router, siteData }) {
     // ...
   }
