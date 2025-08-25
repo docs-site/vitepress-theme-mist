b16f654049f89ae1d9eb40a690b768edf0d7cb65
sumu <2038035593@qq.com>
2025-08-25 21:36:07 +0800

    feat(commit): 🚀 添加Git规范化提交相关工具与脚本

---
+++ .husky/lintstagedrc.js
@@ -0,0 +1,8 @@
+export default {
+  "*.{js,jsx,ts,tsx}": ["echo \"eslint --fix\"", "echo \"prettier --write\""],
+  "!(package)*.json": ["echo \"prettier --write--parser json\""],
+  "package.json": ["echo \"prettier --write\""],
+  "*.vue": ["echo \"eslint --fix", "echo \"prettier --write\""],
+  "*.{vue,css,scss,postcss,less}": ["echo \"prettier --write\""],
+  "*.md": ["echo \"prettier --write\""],
+};
