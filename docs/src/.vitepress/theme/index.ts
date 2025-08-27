2a493fa67555e7d545132a2eb385d82de317d52c
苏木 <sumu@example.com>
2025-08-27 19:18:54 +0800

    refactor(theme): ♻️ 优化侧边栏、添加首页彩虹动画

--- docs/src/.vitepress/theme/index.ts
+++ docs/src/.vitepress/theme/index.ts
@@ -9,6 +9,8 @@ import "./style.css";
 
 // 工作区本地引入
 import "@mist/theme-chalk/mt-plus/fade-up-animation.scss"; // 淡入效果
+import "@mist/theme-chalk/vp-plus/index-rainbow.scss"; // 首页图片彩虹动画
+import "@mist/theme-chalk/vp-plus/sidebar.scss"; // 侧边栏调整
 
 export default {
   extends: Mist,
