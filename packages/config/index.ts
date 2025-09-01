686c4e507b8fd493bf1f000d898d66539128b502
苏木 <sumu@example.com>
2025-09-01 17:36:31 +0800

    refactor(theme): ♻️ 去掉主题增强组件的布局主题色默认配置
    - 防止PC端也会显示一个主题色切换的按钮

--- packages/config/index.ts
+++ packages/config/index.ts
@@ -137,11 +137,11 @@ const defaultMistConfig: Required<MistConfig> = {
     },
     // 布局主题色配置
     themeColor: {
-      disabled: false, // 禁用布局主题色切换
-      defaultColorName: "vp-default", // 布局默认主题色
-      defaultSpread: false, // 是否将主题色扩散到其他元素（根据主题色计算其他元素需要的颜色）
-      disableHelp: false, // 禁用帮助提示
-      disabledInMobile: false, // 是否在移动端禁用
+      // disabled: false, // 禁用布局主题色切换
+      // defaultColorName: "vp-default", // 布局默认主题色
+      // defaultSpread: false, // 是否将主题色扩散到其他元素（根据主题色计算其他元素需要的颜色）
+      // disableHelp: false, // 禁用帮助提示
+      // disabledInMobile: false, // 是否在移动端禁用
     }
   },
   vitePlugins: {
