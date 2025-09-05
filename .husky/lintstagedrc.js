e5ce1dc75adceb02e98e0eb8032a39d178e70833
苏木 <sumu@example.com>
2025-09-05 14:42:25 +0800

    feat(commit): 🚀 提交前对代码格式化和语法检测

--- .husky/lintstagedrc.js
+++ .husky/lintstagedrc.js
@@ -1,8 +1,8 @@
 export default {
-  "*.{js,jsx,ts,tsx}": ['echo "eslint --fix"', 'echo "prettier --write"'],
-  "!(package)*.json": ['echo "prettier --write--parser json"'],
-  "package.json": ['echo "prettier --write"'],
-  "*.vue": ['echo "eslint --fix', 'echo "prettier --write"'],
-  "*.{vue,css,scss,postcss,less}": ['echo "prettier --write"'],
-  "*.md": ['echo "prettier --write"'],
+  "*.{js,jsx,ts,tsx}": ["eslint --fix", "prettier --write"],
+  "!(package)*.json": ["prettier --write--parser json"],
+  "package.json": ["prettier --write"],
+  "*.vue": ["eslint --fix", "prettier --write"],
+  "*.{vue,css,scss,postcss,less}": ["prettier --write"],
+  "*.md": ["prettier --write"],
 };
