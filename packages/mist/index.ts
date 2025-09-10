ec2d02f15cbed55277155868a4e5101e0e0578cb
sumu <2038035593@qq.com>
2025-09-10 18:47:38 +0800

    refactor(theme): ♻️ 优化代码逻辑

--- packages/mist/index.ts
+++ packages/mist/index.ts
@@ -1,4 +1,6 @@
+import type { EnhanceAppContext } from "vitepress";
 import DefaultTheme from "vitepress/theme";
+import { isClient } from "@mist/helper";
 
 import { MtDemoCode, MistConfigProvider, MtLayout } from "@mist/components";
 
@@ -17,7 +19,39 @@ export * from "@mist/components";
 export default {
   extends: DefaultTheme,
   Layout: MistConfigProvider(MtLayout),
-  enhanceApp({ app }) {
+  async enhanceApp({ app, siteData, router }) {
     app.component("MtDemoCode", MtDemoCode);
+    if (!isClient) return;
+    const { themeConfig } = siteData.value;
+    // 处理永久链接导致 404 问题
+    if (themeConfig.permalinks) await processPermalinkNotFoundWhenFirstLoaded({ siteData, router });
   },
-} as DefaultThemeType & { extends: DefaultThemeType };
+} as Omit<DefaultThemeType, "enhanceApp"> & {
+  extends: DefaultThemeType;
+  enhanceApp: (options: EnhanceAppContext) => Promise<void>;
+};
+
+/**
+ * 第一次访问页面时，处理永久链接导致 404 问题
+ */
+const processPermalinkNotFoundWhenFirstLoaded = async ({ siteData, router }: any) => {
+  const { base, cleanUrls, themeConfig } = siteData.value;
+  // 404 页面处理永久链接 404 问题（仅针对首次页面刷新）
+  if (router.route.path === base && router.route.data.isNotFound) {
+    const { pathname, search, hash } = new URL(location.href);
+    const decodePath =
+      "/" +
+      decodeURIComponent(pathname.slice(base.length))
+        .replace(/\/$/, "")
+        .replace(/\.html/, "");
+
+    const link = cleanUrls ? decodePath : decodePath + ".html";
+    const filePath = themeConfig.permalinks.inv[link];
+
+    // 通过永久链接获取的文件路径存在，则跳转
+    if (filePath) {
+      const targetUrl = base + filePath + search + hash;
+      await router.go(targetUrl);
+    }
+  }
+};
