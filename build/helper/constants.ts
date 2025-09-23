b4d86a05563891b2f9db462cc01793e7327caf7a
苏木 <sumu@example.com>
2025-09-23 17:10:33 +0800

    build: 📦️ 新增 simplePkgName 常量用于统一包名前缀

--- build/helper/constants.ts
+++ build/helper/constants.ts
@@ -1,4 +1,5 @@
 export const target = "es2020";
 export const pkgName = "mist";
+export const simplePkgName = "mt";
 export const pkgCamelCaseName = "Mist";
 export const outputPkgName = "vitepress-theme-mist";
